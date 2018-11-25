897531da40fd87c2a547592ecef8f96b1349174a
Elif C <elif.cilingir061@gmail.com>
2018-11-25 14:57:43 +0100

    comment insertion to explain

--- src/Exercice1/Question2/Test.java
+++ src/Exercice1/Question2/Test.java
@@ -1,7 +1,16 @@
 package Exercice1.Question2;
 
+/*
+* Ce programme nous affiche iabde
+* car test fais appel à f()
+ */
 public class Test {
     String string = "i";
+
+    /*
+    * Cette fonction ajoute a et fais appel à g()
+    * la fonction g() ayant une exception nous rentrons dans le catch qui ajoute e
+     */
     void f() {
         try {
             string += "a";
@@ -10,6 +19,10 @@ public class Test {
             string += "e";
         }
     }
+    /*
+    * cette fonction ajoute b et appel h() puis également ajoute d
+    * La fonction h() ayant une exception on rentre dans le catch après
+     */
     void g() throws Exception {
         try {
             string += "b";
@@ -21,6 +34,11 @@ public class Test {
         }
         string += "3";
     }
+
+    /*
+    * Cette fonction retourne une exception
+    *
+     */
     void h() throws Exception {
         throw new Exception();
     }
