71e009ff85efceb714f3c5abbb9b1e8b961d3932
Elif C <elif.cilingir061@gmail.com>
2018-12-13 12:46:25 +0100

    console parameter

--- src/Exercice4/Chaine.java
+++ src/Exercice4/Chaine.java
@@ -1,29 +1,20 @@
 package Exercice4;
 
+import java.util.Scanner;
+
 public class Chaine
 {
-    public static int appartientAlphabet(StringBuilder ch1, StringBuilder ch2, char[] alphabet, StringBuilder chf){
-        int appartien = 0;
-        for(int i = 0; i < chf.length(); i++){
-            for(int j = 0; j < alphabet.length; j++) {
-                if (chf.charAt(i) == alphabet[j]) {
-                    appartien++;
-                    break;
-                }else if(chf.charAt(i) == alphabet[j]+32{
-                    appartien++;
-                    break;
-                }
-            }
-        }
-        return appartien;
+    public static int appartientAlphabet(){
+
+
+        return 0;
 
     }
     public static void main(String[] args){
-        char []alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        StringBuilder ch1 = new StringBuilder("test");
-        StringBuilder ch2 = new StringBuilder("ok");
-        StringBuilder ch3 = new StringBuilder("chaine");
-
-        appartientAlphabet(ch1,ch2,alphabet,ch3);
+        System.out.println(args[0] + " " + args[1] + " " + args[2] + " " + args[3] );
+        String word = "";
+        System.out.print("Word : ");
+        Scanner scWord = new Scanner(System.in);
+        word = scWord.nextLine();
     }
 }
