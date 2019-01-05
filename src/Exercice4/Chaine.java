0c6e19771321c62c51012307c921867575b0d8ce
Elif C <elif.cilingir061@gmail.com>
2019-01-05 23:33:50 +0100

    space

--- src/Exercice4/Chaine.java
+++ src/Exercice4/Chaine.java
@@ -13,7 +13,7 @@ public class Chaine
     public static void main(String[] args){
         System.out.println(args[0] + " " + args[1] + " " + args[2] + " " + args[3] );
         String word = "";
-        System.out.print("Word : ");
+        System.out.print("Word :  ");
         Scanner scWord = new Scanner(System.in);
         word = scWord.nextLine();
     }
